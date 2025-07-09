9f705efc70f283d373249481508fbbf348064e26
Danila267 <SnegirevD505@gmail.com>
2025-07-09 16:10:36 +0500

    HomePrice, Home early layouts

---
+++ src/components/HomePrice.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="homeprice">
+        <div class="homeprice__block">
+            <!-- <img src="" alt="" class="homeprice__block--right"> -->
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: "HomePrice"
+}
+</script>
+
+<style lang="scss" scoped>
+    .homeprice {
+        width: 100%;
+        height: 80vh;
+        position: relative;
+        display: flex;
+        background-color: blue;
+        justify-content: center;
+        align-items: flex-start;
+    }
+    .homeprice__block {
+        width: 1635px;
+        height: 650px;
+        background-color: cadetblue;
+    }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,31 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HomePrice />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HomePrice from '@/components/HomePrice.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    HomePrice
   }
 }
 </script>
+
+<style lang="scss" scoped>
+
+.home {
+  width: 100%;
+  height: auto;
+  top: 11.5em;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  background-color: blue;
+}
+
+</style>
