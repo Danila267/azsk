5d9f0f195b9d5372ee87359230aa86e899747081
Danila267 <SnegirevD505@gmail.com>
2025-07-09 16:59:09 +0500

    HomePrice text, image layouts

--- src/components/HomePrice.vue
+++ src/components/HomePrice.vue
@@ -1,7 +1,12 @@
 <template>
     <div class="homeprice">
         <div class="homeprice__block">
-            <!-- <img src="" alt="" class="homeprice__block--right"> -->
+            <div class="homeprice__block--left">
+                <p class="homeprice__block--left__title">Самые <br>Выгодные<br>Цены</p>
+                <p class="homeprice__block--left__text">Мы предлагаем оборудование для АГЗС и АЗС по лучшим ценам в нашем каталоге</p>
+                <div class="homeprice__block--left__buttonline"></div>
+            </div>
+            <img src="../assets/nasos3.png" alt="" class="homeprice__block--right">
         </div>
     </div>
 </template>
@@ -24,8 +29,48 @@ export default {
         align-items: flex-start;
     }
     .homeprice__block {
-        width: 1635px;
+        width: 1690px;
         height: 650px;
         background-color: cadetblue;
+        position: relative;
+        display: flex;
+    }
+    .homeprice__block--left {
+        background-color: darkred;
+        // flex-grow: 0.6;
+        flex-shrink: 3;
+        display: flex;
+        flex-direction: column;
+        justify-content: flex-start;
+        align-items: flex-start;
+        min-width: 0;
+    }
+    .homeprice__block--left__title {
+        width: 100%;
+        height: auto;
+        position: relative;
+        display: flex;
+        color: #FFFFFF;
+        font-size: 5.4em;
+        font-weight: bold;
+        padding-left: 1.5em;
+        padding-top: 0.35em;
+        box-sizing: border-box;
+    }
+    .homeprice__block--left__text {
+        width: 600px;
+        height: auto;
+        position: relative;
+        display: flex;
+        color: #FFFFFF;
+        font-size: 1.25em;
+        padding-left: 130px;
+        box-sizing: border-box;
+    }
+    .homeprice__block--right {
+        width: 768px;
+        height: auto;
+        object-fit: contain;
+        margin-right: 6em;
     }
 </style>
