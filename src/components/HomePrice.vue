321a9974b9d5925b2dcea00e53a6dffb85479533
Danila267 <SnegirevD505@gmail.com>
2025-07-09 18:38:16 +0500

    HomePrice button layout

--- src/components/HomePrice.vue
+++ src/components/HomePrice.vue
@@ -4,7 +4,14 @@
             <div class="homeprice__block--left">
                 <p class="homeprice__block--left__title">Самые <br>Выгодные<br>Цены</p>
                 <p class="homeprice__block--left__text">Мы предлагаем оборудование для АГЗС и АЗС по лучшим ценам в нашем каталоге</p>
-                <div class="homeprice__block--left__buttonline"></div>
+                <div class="homeprice__block--left__buttonline">
+                    <button class="homeprice__block--left__buttonline__button--first">
+                        КАТАЛОГ
+                    </button>
+                    <button class="homeprice__block--left__buttonline__button--second">
+                        НАПИСАТЬ НАМ
+                    </button>
+                </div>
             </div>
             <img src="../assets/nasos3.png" alt="" class="homeprice__block--right">
         </div>
@@ -43,10 +50,10 @@ export default {
         flex-direction: column;
         justify-content: flex-start;
         align-items: flex-start;
-        min-width: 0;
+        min-width: 800px;
     }
     .homeprice__block--left__title {
-        width: 100%;
+        width: auto;
         height: auto;
         position: relative;
         display: flex;
@@ -58,14 +65,46 @@ export default {
         box-sizing: border-box;
     }
     .homeprice__block--left__text {
-        width: 600px;
+        max-width: 600px;
         height: auto;
         position: relative;
         display: flex;
         color: #FFFFFF;
         font-size: 1.25em;
         padding-left: 130px;
+        padding-top: 15px;
         box-sizing: border-box;
+    }
+    .homeprice__block--left__buttonline {
+        max-width: 700px;
+        height: auto;
+        position: relative;
+        display: flex;
+        margin-left: 105px;
+        margin-top: 60px;
+        background-color: burlywood;
+    }
+    .homeprice__block--left__buttonline__button--first {
+        max-width: 265px;
+        height:80px;
+        align-self: flex-start;
+        font-size: 1.5em;
+        color: #FFFFFF;
+        font-weight: 600;
+        background-color: #8D8CDA;
+        border: 0px #8D8CDA solid;
+        border-radius: 80px;
+    }
+    .homeprice__block--left__buttonline__button--second {
+        max-width: 334px;
+        height: 80px;
+        margin-left: auto;
+        font-size: 1.5em;
+        color: #FFFFFF;
+        font-weight: 600;
+        border: 5px #94CFD0 solid;
+        border-radius: 80px;
+        background-color: #01002F;
     }
     .homeprice__block--right {
         width: 768px;
