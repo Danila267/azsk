3bb780ef120fe16162b708d33f995f1c7f7b9558
Danila267 <SnegirevD505@gmail.com>
2025-08-02 21:58:28 +0500

    Contact item list layout

--- src/views/Contacts.vue
+++ src/views/Contacts.vue
@@ -1,6 +1,22 @@
 <template>
     <div class="contacts">
-        Contact us
+        <div class="contacts__block">
+            <div class="contacts__block__title">Контакты</div>
+            <div class="contacts__block__list">
+                <div class="contacts__block__list__item">
+                    FJHF
+                </div>
+                <div class="contacts__block__list__item">
+                    POKIK
+                </div>
+                <div class="contacts__block__list__item">
+                    aslkamdm
+                </div>
+                <div class="contacts__block__list__item">
+                    qwpoqwij
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -13,12 +29,64 @@ export default {
 <style lang="scss" scoped>
 .contacts {
     width: 100%;
-    height: auto;
+    height: 80vh;
     top: 11.5em;
     position: relative;
     display: flex;
     flex-direction: column;
     //   background-color: blue;
+    justify-content: flex-start;
+    align-items: center;
+    background-color: #01002F;
     z-index: 300;
+}
+.contacts__block {
+    max-width: 1400px;
+    height: auto;
+    // background-color: #afc985;
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    z-index: 300;
+    background-color: aquamarine;
+    align-items: center;
+}
+.contacts__block__title {
+    width: auto;
+    height: auto;
+    position: relative;
+    display: flex;
+    color: #FFFFFF;
+    font-size: 5.4em;
+    font-weight: bold;
+    font-family: 'Outfit', sans-serif;
+    padding-left: 0.35em;
+    padding-top: 0.35em;
+    box-sizing: border-box;
+    right: 5%;
+    //   position: absolute;
+}
+.contacts__block__list {
+    width: 100%;
+    height: auto;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: center;
+    position: relative;
+    background-color: red;
+    margin-top: 2em;
+    gap: 5px;
+}
+.contacts__block__list__item {
+  flex-basis: 500px;
+  height: 154px;
+  background-color: #d2d3e6;
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  transition: all 0.3s ease;
+  overflow: hidden;
+  flex-grow: 1;
 }
 </style>
